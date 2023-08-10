d939d6174a704b3efffe4bf561471b88b94ed0fb
Tianyu Chen <sweetyfish@deepin.org>
2023-08-10 11:17:48 +0800

    feat: qt6-multimedia 6.4.2

--- src/android/jar/src/org/qtproject/qt/android/multimedia/QtMultimediaUtils.java
+++ src/android/jar/src/org/qtproject/qt/android/multimedia/QtMultimediaUtils.java
@@ -1,41 +1,5 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the QtMultimedia of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 3 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL3 included in the
-** packaging of this file. Please review the following information to
-** ensure the GNU Lesser General Public License version 3 requirements
-** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 2.0 or (at your option) the GNU General
-** Public license version 3 or any later version approved by the KDE Free
-** Qt Foundation. The licenses are as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-2.0.html and
-** https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+// Copyright (C) 2016 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 package org.qtproject.qt.android.multimedia;
 
@@ -44,10 +8,14 @@ import android.content.Context;
 import android.media.MediaCodecInfo;
 import android.media.MediaCodecList;
 import android.view.OrientationEventListener;
+import android.webkit.MimeTypeMap;
+import android.net.Uri;
+import android.content.ContentResolver;
 import android.os.Environment;
 import android.media.MediaScannerConnection;
 import java.lang.String;
 import java.io.File;
+import android.util.Log;
 
 public class QtMultimediaUtils
 {
@@ -72,6 +40,7 @@ public class QtMultimediaUtils
 
     static private Context m_context = null;
     static private OrientationListener m_orientationListener = null;
+    private static final String QtTAG = "Qt QtMultimediaUtils";
 
     static public void setActivity(Activity qtMainActivity, Object qtActivityDelegate)
     {
@@ -138,6 +107,8 @@ public class QtMultimediaUtils
         MediaScannerConnection.scanFile(m_context, new String[] { file }, null, null);
     }
 
+    static File getCacheDirectory() { return m_context.getCacheDir(); }
+
     /*
     The array of codecs is in the form:
         c2.qti.vp9.decoder
@@ -153,4 +124,25 @@ public class QtMultimediaUtils
             codecs[i] = codecInfoArray[i].getName();
         return codecs;
     }
+
+public static String getMimeType(Context context, String url)
+{
+    Uri parsedUri = Uri.parse(url);
+    String type = null;
+
+    try {
+        String scheme = parsedUri.getScheme();
+        if (scheme != null && scheme.contains("content")) {
+            ContentResolver cR = context.getContentResolver();
+            type = cR.getType(parsedUri);
+        } else {
+            String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+            if (extension != null)
+                type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+       }
+    } catch (Exception e) {
+        Log.e(QtTAG, "getMimeType(): " + e.toString());
+    }
+    return type;
+}
 }
